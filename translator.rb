0aa2595fbd7fcbe18425721af5ada14d98a82c8b
Narada Jayasingha <narada@venturit.com>
2015-06-10 01:52:07 -0500

    Define a class and manage zero output

--- translator.rb
+++ translator.rb
@@ -1,5 +1,22 @@
+class ArabicNumeralToEnglish            
+  def translator(numerals)
+    numerals = numerals.to_i.abs
+    
+    if numerals == 0
+      return "zero" 
+    else
+      #manage rest of the numbers here
+    end
+
+  end
+end
+
 unless ARGV[0].nil?
-  puts ARGV[0]
+  object = ArabicNumeralToEnglish. new
+  english_wordings = object.translator(ARGV[0])
+  
+  # display translated output 
+  puts english_wordings
 else
   puts "Please run this program with arabic numerals as first argument!"
 end
